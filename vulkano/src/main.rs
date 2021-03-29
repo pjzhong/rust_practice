192c307844ba12c7f8e583852348b5de2aaae165
pj_zhong <pj_zhong@163.com>
2021-03-29 21:17:37 +0800

    [vulkano] compute operation

--- vulkano/src/main.rs
+++ vulkano/src/main.rs
@@ -1,10 +1,13 @@
 use std::sync::Arc;
 
+use vulkano::command_buffer::AutoCommandBuffer;
 use vulkano::{
     buffer::{BufferUsage, CpuAccessibleBuffer},
     command_buffer::{AutoCommandBufferBuilder, CommandBuffer},
+    descriptor::{descriptor_set::PersistentDescriptorSet, PipelineLayoutAbstract},
     device::{Device, DeviceExtensions, Features, Queue},
     instance::{Instance, InstanceExtensions, PhysicalDevice},
+    pipeline::ComputePipeline,
     sync::GpuFuture,
 };
 
@@ -47,6 +50,60 @@ fn main() {
         CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, data_iter)
             .expect("failed to create buffer");
 
+    let shader = cs::Shader::load(device.clone()).expect("failed to create shader module");
+    let compute_pipeline = Arc::new(ComputePipeline::new(
+        device.clone(),
+        &shader.main_entry_point(),
+        &(),
+        Option::None,
+    ))
+    .expect("failed to create compute pipline");
+
+    let layout = compute_pipeline.layout().descriptor_set_layout(0).unwrap();
+    let set = Arc::new(
+        PersistentDescriptorSet::start(layout.clone())
+            .add_buffer(data_buffer.clone())
+            .unwrap()
+            .build()
+            .unwrap(),
+    );
+
+    let mut builder = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap();
+    builder
+        .dispatch([1024, 1, 1], compute_pipeline.clone(), set.clone(), (), 0)
+        .unwrap();
+    let command_buffer = builder.build().unwrap();
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished
+        .then_signal_fence_and_flush()
+        .unwrap()
+        .wait(None)
+        .unwrap();
+
+    let content = data_buffer.read().unwrap();
+    for (n, val) in content.iter().enumerate() {
+        assert_eq!(val, n as u32 * 12)
+    }
+}
+
+mod cs {
+    vulkano_shaders::shader! {
+    ty:"compute",
+    src: "
+ #version 450
+
+ layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+
+ layout(set = 0, binding = 0) buffer Data {
+     uint data[];
+ } buf;
+
+ void main() {
+     uint idx = gl_GlobalInvocationID.x;
+     buf.data[idx] *= 12;
+ }
+ "
+    }
 }
 
 #[allow(dead_code)]
