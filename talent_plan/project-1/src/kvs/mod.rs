481f3948f8027ae8c7dd62aa4931b182c0a85f48
pj_zhong <pj_zhong@163.com>
2021-06-26 17:44:56 +0800

    [talent-plan] project_1

--- talent_plan/project-1/src/kvs/mod.rs
+++ talent_plan/project-1/src/kvs/mod.rs
@@ -1,21 +1,33 @@
-pub struct KvStore {
+use std::collections::HashMap;
+use std::hash::Hash;
 
+pub struct KvStore<K, V> {
+    map: HashMap<K, V>,
 }
 
-impl KvStore {
+impl<K, V> KvStore<K, V>
+where
+    K: Eq + Hash,
+    V: Clone,
+{
     pub fn new() -> Self {
-        unimplemented!()
+        Self {
+            map: HashMap::new(),
+        }
     }
 
-    pub fn get(&self, key: String) -> Option<String> {
-        unimplemented!()
+    pub fn get(&self, key: K) -> Option<V> {
+        match self.map.get(&key) {
+            Some(s) => Some(s.clone()),
+            _ => None,
+        }
     }
 
-    pub fn set(&mut self, key : String, val: String) {
-        unimplemented!()
+    pub fn set(&mut self, key: K, val: V) {
+        self.map.insert(key, val);
     }
 
-    pub fn remove(&mut self, key: String) {
-        unimplemented!()
+    pub fn remove(&mut self, key: K) -> Option<V> {
+       self.map.remove(&key)
     }
 }
