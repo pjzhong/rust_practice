da556edd1e01479537a63b6c160bbcb285e0e115
pj_zhong <pj_zhong@163.com>
2021-12-19 12:16:51 +0800

    【algorithms】product_of_array_except_self

--- algorithms/src/algorithms/product_of_array_except_self.rs
+++ algorithms/src/algorithms/product_of_array_except_self.rs
@@ -7,24 +7,20 @@ impl Solution {
     /// [`here`]: https://leetcode.com/problems/product-of-array-except-self/
     #[allow(dead_code)]
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut prefix_sum = Vec::from(nums.as_slice());
-        let mut sufix_sum = Vec::from(nums.as_slice());
-        let mut left = 1;
-        let mut right = nums.len() - 1;
-        for _ in 1..nums.len() {
-            prefix_sum[left] *= prefix_sum[left - 1];
-            sufix_sum[right - 1] *= sufix_sum[right];
-            left += 1;
-            right -= 1;
+        let len = nums.len();
+        let mut res = Vec::from(nums.as_slice());
+        //calc the suffix sume first
+        for i in (1..len).rev() {
+            res[i - 1] *= res[i];
         }
 
-
-        let mut res = vec![0; nums.len()];
-        res[0] = sufix_sum[1];
-        res[nums.len() - 1] = prefix_sum[nums.len() - 2];
-        for i in 1..nums.len() - 1 {
-            res[i] = prefix_sum[i - 1] * sufix_sum[i + 1];
+        res[0] = res[1];
+        let mut prefix_sum = nums[0];
+        for i in 1..len - 1 {
+            res[i] = prefix_sum * res[i + 1];
+            prefix_sum *= nums[i];
         }
+        res[len - 1] = prefix_sum;
 
         res
     }
