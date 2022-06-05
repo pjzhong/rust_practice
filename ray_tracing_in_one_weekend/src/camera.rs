c56d0cebe1b8e27eeb1d1d1efee28cd9536c5234
pj_zhong <pj_zhong@163.com>
2022-06-05 21:01:09 +0800

    [ray_tracing_in_one_weekend] defocus Blur

--- ray_tracing_in_one_weekend/src/camera.rs
+++ ray_tracing_in_one_weekend/src/camera.rs
@@ -6,13 +6,19 @@ pub struct Camera {
     lower_left_corner: Vec3<f32>,
     horizontal: Vec3<f32>,
     vertical: Vec3<f32>,
+    u: Vec3<f32>,
+    v: Vec3<f32>,
+    lens_radius: f32,
 }
 
 impl Camera {
     pub fn get_ray(&self, s: f32, t: f32) -> Ray {
+        let rd = self.lens_radius * Vec3::<f32>::random_in_unit_disk();
+        let offset = self.u * rd.x + self.v * rd.y;
+
         Ray::new(
-            self.origin,
-            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin,
+            self.origin + offset,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin - offset,
         )
     }
 }
@@ -24,6 +30,8 @@ impl Camera {
         vup: Vec3<f32>,
         vfov: f32,
         aspect_ration: f32,
+        aperture: f32,
+        focus_dist: f32,
     ) -> Self {
         let theta = vfov.to_radians();
         let h = (theta / 2.0).tan();
@@ -35,15 +43,20 @@ impl Camera {
         let v = w.cross(&u);
 
         let origin = look_from;
-        let horizontal = viewport_width * u;
-        let vertical = viewport_height * v;
-        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - w;
+        let horizontal = focus_dist * viewport_width * u;
+        let vertical = focus_dist * viewport_height * v;
+        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - focus_dist * w;
+
+        let lens_radius = aperture / 2.0;
 
         Self {
             origin,
             lower_left_corner,
             horizontal,
             vertical,
+            u,
+            v,
+            lens_radius,
         }
     }
 }
--- ray_tracing_in_one_weekend/src/main.rs
+++ ray_tracing_in_one_weekend/src/main.rs
@@ -98,12 +98,19 @@ fn main() {
     ];
 
     // Camera
+    let look_from = Vec3::f32(-2.0, 2.0, 1.0);
+    let look_at = Vec3::f32(0.0, 0.0, -1.0);
+    let vup = Vec3::f32(0.0, 1.0, 0.0);
+    let dist_to_focus = (look_from - look_at).length();
+    let aperture = 2.0;
     let camera = Camera::new(
-        Vec3::f32(-2.0, 2.0, 1.0),
-        Vec3::f32(0.0, 0.0, -1.0),
-        Vec3::f32(0.0, 1.0, 0.0),
+        look_from,
+        look_at,
+        vup,
         90.0,
         aspect_ration,
+        aperture,
+        dist_to_focus,
     );
 
     // Render
